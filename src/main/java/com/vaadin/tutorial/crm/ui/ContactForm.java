3da0c49f533d3ce457f2212dff5e839bb9a37f48
huynino96 <huynino96@gmail.com>
2020-11-27 14:38:41 +0700

    Passing Data and Event

--- src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
@@ -1,6 +1,8 @@
 package com.vaadin.tutorial.crm.ui;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -11,6 +13,8 @@ import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
 
@@ -29,14 +33,15 @@ public class ContactForm extends FormLayout {
     Button close = new Button("Cancel");
 
     Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+    private Contact contact;
 
     public ContactForm(List<Company> companies) {
         addClassName("contact-form");
-        binder.bindInstanceFields(this);
 
+        binder.bindInstanceFields(this);
+        status.setItems(Contact.Status.values());
         company.setItems(companies);
         company.setItemLabelGenerator(Company::getName);
-        status.setItems(Contact.Status.values());
 
         add(
                 firstName,
@@ -48,6 +53,11 @@ public class ContactForm extends FormLayout {
         );
     }
 
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        binder.readBean(contact);
+    }
+
     private Component createButtonsLayout() {
         save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
@@ -56,6 +66,60 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         close.addClickShortcut(Key.ESCAPE);
 
+        save.addClickListener(click -> validateAndSave());
+        delete.addClickListener(click -> fireEvent(new DeleteEvent(this, contact)));
+        close.addClickListener(click -> fireEvent(new CloseEvent(this)));
+
+        binder.addStatusChangeListener(evt -> save.setEnabled(binder.isValid()));
+
         return new HorizontalLayout(save, delete, close);
+    }
+
+    private void validateAndSave() {
+
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+                                                                  ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
     }
 }
